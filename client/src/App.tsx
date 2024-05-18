668dc3d3e7b64ae95781e31de8d855266c2acd65
GurkiratSingh111 <sgurkirat868@gmail.com>
2024-05-18 16:48:08 -0700

    Added login and signup routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Authentication from "./pages/Authentication";
+import Login from "./pages/Login";
 import AuthProvider from "./contexts/auth/AuthProvider";
 import CreateInterview from "./pages/CreateInterview";
 import InterviewsProvider from "./contexts/interviews/InterviewProvider";
+import Signup from "./pages/Signup";
+
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <InterviewsProvider>
       <Router>
         <Routes>
-          <Route path="/auth" element={<Authentication />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/interviews/create" element={<CreateInterview />} />
         </Routes>
       </Router>
