b1c100310b2bcaccc59f6fd65ecf948aeee55740
Salman Ayaz <salmanayazz@gmail.com>
2024-05-19 05:15:10 -0700

    fixed interview component url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/interviews/create" element={<CreateInterview />} />
-              <Route path="/interviews" element={<Interview />} />
+              <Route path="/interviews/:interviewId" element={<Interview />} />
             </Route>
           </Routes>
         </Router>
