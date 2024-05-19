e483bc2a68211746147e17f54788fcf6284b131f
GurkiratSingh111 <sgurkirat868@gmail.com>
2024-05-18 22:28:08 -0700

    Created a component that converts speech to text

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar"
+import SpeechRecognition from "../components/SpeechRecognition"
 
 const Homes = () => {
   return (
     <>
       <Navbar/>
+      <SpeechRecognition/>
     </>
   )
 }
