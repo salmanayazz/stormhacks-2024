5aa355c82a0f08c527e8ac1f3923b9c79235ba68
Salman Ayaz <salmanayazz@gmail.com>
2024-05-19 00:23:30 -0700

    removed unused components

--- client/src/components/Header1.tsx
+++
@@ -1,9 +0,0 @@
-interface Header1Props {
-  text: string;
-}
-
-export default function Header1({ text }: Header1Props) {
-  return (
-    <h1>{text}</h1>
-  );
-}
--- client/src/components/Paragraph.tsx
+++
@@ -1,7 +0,0 @@
-interface ParagraphProps {
-  text: string;
-}
-
-export default function Paragraph({ text }: ParagraphProps) {
-  return <p>{text}</p>;
-}
