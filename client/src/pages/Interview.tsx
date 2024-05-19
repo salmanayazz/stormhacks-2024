27befd48b231f3756d8b06bdba223ea48e1f26e6
Salman Ayaz <salmanayazz@gmail.com>
2024-05-19 01:08:29 -0700

    styled interview page

--- client/src/pages/Interview.tsx
+++ client/src/pages/Interview.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
-import { useInterviews } from "../contexts/interviews/InterviewContext";
+import { Question, useInterviews } from "../contexts/interviews/InterviewContext";
+import { Box, Heading, Textarea, Button, Text } from "@chakra-ui/react";
 
 export default function Interview() {
   const { interviewId } = useParams();
@@ -8,24 +9,25 @@ export default function Interview() {
   const interview = interviews.find((interview) => interview._id === interviewId);
   
   return(
-    <div
-      className="flex flex-col"
-    >
-      <h1>{interview?.position}</h1>
-      <h2>{interview?.company}</h2>
+    <Box p={4}>
+      <Heading mb={4}>{interview?.position}</Heading>
+      <Heading as="h2" size="lg" mb={4}>{interview?.company}</Heading>
 
-      {interview?.info.map((question) => {
+      {interview?.info.map((question: Question) => {
         return (
-          <div key={question._id}>
-            <h3>{question.question}</h3>
-            <textarea
+          <Box key={question._id} mb={6}>
+            <Heading as="h3" size="sm" mb={2}>{question.question}</Heading>
+            <Textarea
               value={question.answer}
+              mb={2}
+              variant="filled"
+              colorScheme="primary"
             />
-            <button>Submit</button>
-            {question.feedback || question.feedback !== "" && <p>{question.feedback}</p>}
-          </div>
+            <Button colorScheme="blue" variant="solid" mr={2}>Submit</Button>
+            <Text>{question.feedback}</Text>
+          </Box>
         );
       })}
-    </div>
+    </Box>
   );
 }
